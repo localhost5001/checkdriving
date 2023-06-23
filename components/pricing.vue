2c2e2d0e1d2d90261f3421bfc669399e195b9d5a
Leo Kim <e_leokim@yahoo.com>
2023-06-22 19:52:40 -0700

    Add pricing component

---
+++ components/pricing.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="text-white">
+    <div class="text-center m-2">
+      <h1 class="text-4xl font-semibold leadi">Pricing</h1>
+    </div>
+
+    <div 
+      class="
+          grid 
+          gap-6 grid-cols-1 
+          lg:gap-2 lg:grid-cols-3
+          m-2
+        "
+      >
+      <pricingCard 
+        v-for="card in cards" 
+        :key="card.title" 
+        :title="card.title"
+        :rates="card.rates"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import pricingCard from './pricingCard.vue'
+import type { PricingCardViewModel } from '@/models/pricingCardViewModel'
+
+const cards: PricingCardViewModel[] = [
+  {
+    title: 'Individual',
+    rates: [
+      { amount: 60, description: '1 Hour' },
+      { amount: 90, description: '1.5 Hours' },
+      { amount: 120, description: '2 Hours' },
+    ],
+  },
+  {
+    title: '1 Hour Packages',
+    rates: [
+      { amount: 300, description: '5 Lessons', discount: 10 },
+      { amount: 600, description: '10 Lessons', discount: 10 },
+      { amount: 900, description: '15 Lessons', discount: 10 },
+    ],
+  },
+  {
+    title: '1/2 Hour Packages',
+    rates: [
+      { amount: 450, description: '5 Lessons', discount: 10 },
+      { amount: 900, description: '10 Lessons', discount: 10 },
+      { amount: 1350, description: '15 Lessons', discount: 10 },
+    ],
+  },
+]
+
+</script>
---
+++ components/pricingCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="
+      felx flex-col overflow-hidden border-2 rounded-md 
+      border-gray-700 p-5"
+    >
+    <div class="text-center m-5">
+      <span class="text-2xl font-bold">{{ title }}</span>
+    </div>
+    <div class="py-4">
+      <ul>
+        <li v-for="rate in rates">
+          <div class="divider"></div>
+          <div class="grid grid-cols-2 place-content-between">
+            <span class="text-left mx-1">
+              {{ rate.description }}
+            </span>
+            
+              <div class="text-right mx-1">
+              <span class="badge badge-primary">
+                ${{ priceWithDiscount(rate.amount, rate.discount) }}
+              </span>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Rate } from '@/models/rate'
+
+const {title} = defineProps<{
+  title: string
+  rates: Rate[]
+}>()
+
+const priceWithDiscount = (amount: number, discount?: number) => {
+  return discount ? amount - (amount * discount) / 100 : amount
+}
+
+</script>
