d6fd537cef6156d1e21fe45728043526f2df7301
Leo Kim <e_leokim@yahoo.com>
2023-06-22 19:52:34 -0700

    Add navbar component

---
+++ components/navBar.vue
@@ -0,0 +1,14 @@
+<template>
+<nav class="navbar md:flex-row flex-col shadow-xl">
+  <div class="flex-1">
+    <a class="btn btn-ghost normal-case text-xl lg:visible">Check Driving School</a>
+  </div>
+  
+  <div class="flex-none">
+    <span class="font-bold text-lg">Contact:</span>
+    <a href="tel:+16047254935" class="text-blue-500 font-medium mx-1 normal-case text-base">
+      604 725 4935
+    </a>
+  </div>
+</nav>
+</template>
