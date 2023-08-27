726bc991b6bb6572e285045ad7a7a09cb9f9706f
Leo Kim <e_leokim@yahoo.com>
2023-08-26 17:48:06 -0700

    add contact info on greetingsPage

--- components/greetingPage.vue
+++ components/greetingPage.vue
@@ -6,6 +6,11 @@
       <h1 class="mb-5 text-5xl font-bold">Check Driving School</h1>
       <p class="mb-5 font-semibold text-lg">Master the Art of Driving, Unleash Your Freedom!</p>
       <button class="btn btn-primary" @click="emit('scrollToPrice')">Rates & Pricing</button>
+      <p class="font-semibold text-lg">Contact us at:</p>
+      <span>Phone: </span>
+      <a href="tel:+16047254935" class="text-blue-500 font-medium mx-1 normal-case text-base">
+        604 725 4935
+      </a>
     </div>
   </div>
 </div>
