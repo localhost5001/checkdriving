c9cb7c074f890582921b5d8e58fb7546c8634164
Leo Kim <e_leokim@yahoo.com>
2023-08-26 17:42:42 -0700

    Simplify navbar

--- components/navBar.vue
+++ components/navBar.vue
@@ -6,19 +6,9 @@
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
       <ul class="shadow menu dropdown-content bg-base-100 rounded-box w-52">
-        <li>
-          <a href="/" class="text-blue-500 font-medium mx-1 normal-case text-base">
-            Home
-          </a>
-        </li>
-        <li>
-          <a href="/about" class="text-blue-500 font-medium mx-1 normal-case text-base">
-            About
-          </a>
-        </li>
-        <li>
-          <a href="/contact" class="text-blue-500 font-medium mx-1 normal-case text-base">
-            Contact
+        <li v-for="link in links">
+          <a :href=link.href class="text-blue-500 font-medium mx-1 normal-case text-base">
+            {{ link.name }}
           </a>
         </li>
       </ul>
@@ -27,19 +17,9 @@
   </div>
   <div class="navbar-center hidden lg:flex">
     <ul class="menu menu-horizontal px-1">
-      <li>
-        <a href="/" class="text-blue-500 font-medium mx-1 normal-case text-base">
-          Home
-        </a>
-      </li>
-      <li>
-        <a href="/about" class="text-blue-500 font-medium mx-1 normal-case text-base">
-          About
-        </a>
-      </li>
-      <li>
-        <a href="/contact" class="text-blue-500 font-medium mx-1 normal-case text-base">
-          Contact
+      <li v-for="link in links">
+        <a :href=link.href class="text-blue-500 font-medium mx-1 normal-case text-base">
+          {{ link.name }}
         </a>
       </li>
     </ul>
