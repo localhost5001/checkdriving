3f2d17def3839aa72129151531a3b423146c9df7
Leo Kim <e_leokim@yahoo.com>
2023-08-26 19:34:13 -0700

    Fix navbar on mobile screens

--- components/navBar.vue
+++ components/navBar.vue
@@ -1,21 +1,9 @@
 <template>
-<nav class="navbar ">
+<nav class="navbar flex flex-col md:flex-row">
   <div class="navbar-start">
-    <div class="dropdown">
-      <label tabindex="0" class="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-      </label>
-      <ul class="shadow menu dropdown-content bg-base-100 rounded-box w-52">
-        <li v-for="link in links">
-          <a :href=link.href class="text-blue-500 font-medium mx-1 normal-case text-base">
-            {{ link.name }}
-          </a>
-        </li>
-      </ul>
-    </div>
     <a class="btn btn-ghost normal-case text-xl">Check Driving School</a>
   </div>
-  <div class="navbar-center hidden lg:flex">
+  <div class="navbar-center ">
     <ul class="menu menu-horizontal px-1">
       <li v-for="link in links">
         <a :href=link.href class="text-blue-500 font-medium mx-1 normal-case text-base">
