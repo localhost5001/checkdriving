c65c7c4d3090181451de7af0278de533fbf72a73
Leo <e_leokim@yahoo.com>
2023-11-19 20:04:29 -0800

    add pricing button

--- components/greetingPage.vue
+++ components/greetingPage.vue
@@ -13,7 +13,15 @@
       <p class="text-lg text-gray-600">Welcome to Check Driving School - Your Path to Safe and Confident Driving!</p>
     </div>
 
+    
+
     <div class="mt-8 gap-3 flex justify-center">
+      <a @click="() => emit('scrollToPrice')" class="inline-flex justify-center items-center gap-x-3 text-center bg-gradient-to-tl from-green-600 to-teal-600 hover:from-teal-600 hover:to-green-600 border border-transparent text-white text-sm font-medium rounded-md focus:outline-none focus:ring-1 focus:ring-gray-600 py-3 px-4" href="#">
+        Pricing
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z" />
+        </svg>
+      </a>
       <a href="tel:+16047254935" class="relative group p-2 ps-3 inline-flex items-center gap-x-2 text-sm font-mono rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none">
         604 725 4935
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
@@ -21,13 +29,13 @@
         </svg>
       </a>
     </div>
-
     <ProgramExperience />
-    
   </div>
 </div>
 </template>
 
 <script setup lang="ts">
 import ProgramExperience from './programExperience.vue'
+
+const emit = defineEmits(['scrollToPrice'])
 </script>
