200624c8dd2979e52fdeb395960bc20ecae8c930
Leo <e_leokim@yahoo.com>
2023-11-19 18:31:51 -0800

    update navbar

--- components/navBar.vue
+++ components/navBar.vue
@@ -1,27 +1,44 @@
 <template>
-<nav class="navbar flex flex-col md:flex-row">
-  <div class="navbar-start">
-    <a class="btn btn-ghost normal-case text-xl">Check Driving School</a>
-  </div>
-  <div class="navbar-center ">
-    <ul class="menu menu-horizontal px-1">
-      <li v-for="link in links">
-        <a :href=link.href class="text-blue-500 font-medium mx-1 normal-case text-base">
+<header class="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-white text-sm py-4">
+  <nav class="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between" aria-label="Global">
+    <div class="flex items-center justify-between">
+      <a class="inline-flex items-center gap-x-2 text-xl font-semibold" href="#">
+        <svg 
+          xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.7" stroke="currentColor" 
+          class="w-10 h-10 text-teal-600"
+        >
+          <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+        </svg>
+        <span class="text-gray-700">Check Driving School</span>
+      </a>
+      <div class="sm:hidden">
+        <button type="button" class="hs-collapse-toggle p-2 inline-flex justify-center items-center gap-x-2 rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none" data-hs-collapse="#navbar-image-and-text-1" aria-controls="navbar-image-and-text-1" aria-label="Toggle navigation">
+          <svg class="hs-collapse-open:hidden flex-shrink-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="3" x2="21" y1="6" y2="6"/><line x1="3" x2="21" y1="12" y2="12"/><line x1="3" x2="21" y1="18" y2="18"/></svg>
+          <svg class="hs-collapse-open:block hidden flex-shrink-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
+        </button>
+      </div>
+    </div>
+    <div id="navbar-image-and-text-1" class="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow sm:block">
+      <div class="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:ps-5">
+        <a 
+          v-for="link in links" :key="link.href"
+          class="font-medium"
+          :class="{ 'text-blue-500': route.path === link.href, 'text-gray-600 hover:text-gray-400': route.path !== link.href }" 
+          :href="link.href" 
+          :area-current="route.path === link.href ? 'page' : null">
           {{ link.name }}
         </a>
-      </li>
-    </ul>
-  </div>
-  <div class="navbar-end hidden lg:flex">
-    <span class="font-bold text-lg">Contact:</span>
-    <a href="tel:+16047254935" class="text-blue-500 font-medium mx-1 normal-case text-base">
-      604 725 4935
-    </a>
-  </div>
-</nav>
+      </div>
+    </div>
+  </nav>
+</header>
 </template>
 
 <script setup lang="ts">
+import { useRoute } from 'nuxt/app'
+
+const route = useRoute()
+
 const links = [
   {
     name: 'Home',
