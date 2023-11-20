78646993788770ef9acab7384ff2fe22ddd45018
Leo <e_leokim@yahoo.com>
2023-11-19 19:46:35 -0800

    Update program experience component

--- components/programExperience.vue
+++ components/programExperience.vue
@@ -1,31 +1,45 @@
 <template>
-<div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-  <h5 class="mb-2 mb-8 text-4xl font-extrabold leading-none md:pl-2">
-    Program Experience
-  </h5>
-  <div class="grid gap-5 row-gap-5 sm:grid-cols-2 lg:grid-cols-4">
-    <div v-for="item in items" class="px-12 text-center sm:px-0">
-      <div class="flex items-center justify-center w-10 h-10 mx-auto mb-4 rounded-full bg-blue-500 sm:w-12 sm:h-12">
-        <svg fill="#000000" class="fill-slate-300" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><path d="M5 16.577l2.194-2.195 5.486 5.484L24.804 7.743 27 9.937l-14.32 14.32z"/></svg>
+<!-- Icon Blocks -->
+<div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+  <div class="max-w-2xl mx-auto">
+    <!-- Grid -->
+    <div class="grid gap-12">
+      <div>
+        <h2 class="text-3xl text-gray-800 font-bold lg:text-4xl text-center">
+          Program Experience
+        </h2>
+        <p class="mt-3 text-lg text-gray-600 text-center">
+          We offer top-notch driving education with a focus on your comfort and success. Our driving school features:
+        </p>
       </div>
-      <h6 class="mb-2 text-sm font-bold leading-5 tracking-wider uppercase">
-        {{ item.title }}
-      </h6>
-      <div class="mb-2 ">
-        {{ item.description }}
+
+      <div class="grid grid-cols-2 gap-5">
+        <ProgramExperienceItem 
+          v-for="item in items" 
+          :key="item.title" 
+          :title="item.title"
+          :description="item.description"
+        />
       </div>
     </div>
+    <!-- End Grid -->
   </div>
 </div>
+<!-- End Icon Blocks -->
 </template>
 
 <script setup lang="ts">
+import ProgramExperienceItem from '@/components/programExperienceItem.vue'
+
 const items = [
   { 
     title: 'Dual-Pedal Automatic Car', 
     description: 'Master driving in a secure environment' 
   },
-  { title: 'Bilingual Instruction', description: 'Russian, English' },
+  { 
+    title: 'Bilingual Instruction', 
+    description: 'Russian, English' 
+  },
   { 
     title: 'Flexible Schedule', 
     description: 'Day or night, weekends, holidays-year-round' 
---
+++ components/programExperienceItem.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="text-center">
+  <div class="flex justify-center items-center w-12 h-12 bg-gray-50 border border-gray-200 rounded-full mx-auto">
+    <svg 
+      class="flex-shrink-0 w-5 h-5 text-teal-600 " xmlns="http://www.w3.org/2000/svg" 
+      width="24" height="24" viewBox="0 0 24 24" 
+      fill="none" stroke="currentColor" stroke-width="3" 
+      stroke-linecap="round" stroke-linejoin="round">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+    </svg>
+  </div>
+  <div class="mt-3">
+    <h3 class="text-lg font-semibold text-gray-800">{{ title }}</h3>
+    <p class="mt-1 text-gray-600">{{ description }}</p>
+  </div>
+</div>  
+</template>
+
+<script setup lang="ts">
+const {title} = defineProps<{
+  title: string
+  description: string
+}>()
+</script>
