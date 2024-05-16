318e218bd234712328d713cdeeef62d2f840905b
Leo <leonidk101@icloud.com>
2024-05-15 23:17:37 -0700

    Rename Packages to Prepaid Package of Lessons

--- components/pricing.vue
+++ components/pricing.vue
@@ -35,7 +35,7 @@ const cards: PricingCardViewModel[] = [
     ],
   },
   {
-    title: 'Packages (Each Class is 1.5 Hours)',
+    title: 'Prepaid Package of Lessons (Each Class is 1.5 Hours)',
     rates: [
       { amount: 950, description: '10 Classes (50$ discount)' },
       { amount: 1800, description: '20 Classes (200$ discount)' },
