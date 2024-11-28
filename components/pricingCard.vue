5a6eb349636cbb3b326fa6de1e74c7f781821a9c
Elina Kim <elinakim791@gmail.com>
2024-11-28 13:48:21 -0800

    modify styling and add note fields

--- components/pricingCard.vue
+++ components/pricingCard.vue
@@ -1,32 +1,29 @@
 <template>
-  <div 
-    class="
-      flex flex-col 
-      bg-white border 
-      border-gray-200 
-      shadow-sm 
-      rounded-xl 
-      p-4 
-      md:p-5"
-    >
-    <div class="text-center m-5">
+  <div
+    class="flex flex-col bg-white border border-gray-200 shadow-sm rounded-xl p-4 md:p-5"
+  >
+    <div class="text-center m-4 h-20">
       <span class="text-2xl font-bold text-gray-800">{{ title }}</span>
+      <p class="text-sm font-bold text-gray-500 mt-2">{{ note }}</p>
     </div>
     <div class="py-4">
       <ul>
-        <li v-for="rate in rates">
+        <li v-for="rate in rates" class="mb-4 h-20">
           <div class="divider"></div>
-          <div class="grid grid-cols-2 place-content-between">
-            <span class="text-left mx-1">
+          <div class="grid grid-cols-2 items-center">
+            <span class="text-left">
               {{ rate.description }}
             </span>
-            
-              <div class="text-right mx-1">
+
+            <div class="text-right">
               <span class="badge">
-                {{ priceWithDiscount(rate.amount, rate.discount) }}$
+                ${{ priceWithDiscount(rate.amount, rate.discount) }}
               </span>
             </div>
           </div>
+          <p v-if="rate.note" class="text-sm text-gray-500 mt-1">
+            {{ rate.note }}
+          </p>
         </li>
       </ul>
     </div>
@@ -34,15 +31,15 @@
 </template>
 
 <script setup lang="ts">
-import type { Rate } from '@/models/rate'
+import type { Rate } from "@/models/rate";
 
-const {title} = defineProps<{
-  title: string
-  rates: Rate[]
-}>()
+const { title, note } = defineProps<{
+  title: string;
+  note?: string;
+  rates: (Rate & { note?: string })[];
+}>();
 
 const priceWithDiscount = (amount: number, discount?: number) => {
-  return discount ? amount - (amount * discount) / 100 : amount
-}
-
+  return discount ? amount - (amount * discount) / 100 : amount;
+};
 </script>
