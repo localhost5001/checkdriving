8f46b7d6fe6244a48829e40f6238a7bd6288e9e5
Elina Kim <40552613+ElinaKim@users.noreply.github.com>
2024-11-28 16:28:10 -0800

    Forked: Pricing card change  (#6)
    * add note field to rate model
    
    * modify styling and add note fields
    
    * change pricing and add note fields

--- components/pricing.vue
+++ components/pricing.vue
@@ -1,21 +1,17 @@
 <template>
   <div>
     <div class="text-center m-2">
-      <h1 class="text-3xl text-gray-800 font-bold lg:text-4xl text-center">Pricing</h1>
+      <h1 class="text-3xl text-gray-800 font-bold lg:text-4xl text-center">
+        Pricing
+      </h1>
     </div>
 
-    <div 
-      class="
-          grid gap-2
-          grid-cols-1
-          lg:gap-2 lg:grid-cols-3
-          m-2
-        "
-      >
-      <pricingCard 
-        v-for="card in cards" 
-        :key="card.title" 
+    <div class="grid gap-2 grid-cols-1 lg:gap-2 lg:grid-cols-3 m-2">
+      <pricingCard
+        v-for="card in cards"
+        :key="card.title"
         :title="card.title"
+        :note="card.note"
         :rates="card.rates"
       />
     </div>
@@ -23,31 +19,37 @@
 </template>
 
 <script setup lang="ts">
-import pricingCard from './pricingCard.vue'
-import type { PricingCardViewModel } from '@/models/pricingCardViewModel'
+import pricingCard from "./pricingCard.vue";
+import type { PricingCardViewModel } from "@/models/pricingCardViewModel";
 
 const cards: PricingCardViewModel[] = [
   {
-    title: 'Regular Single Lessons',
+    title: "Regular Single Lessons",
     rates: [
-      { amount: 80, description: '1 Hour' },
-      { amount: 100, description: '1.5 Hours' }
+      {
+        amount: 80,
+        description: "1 Hour",
+        note: "Everywhere in Lower Mainlaind.",
+      },
+      { amount: 100, description: "1.5 Hours", note: "Everywhere in Lower Mainlaind.",},
+      { amount: 65, description: "1 Hour", note: "* Lansdowne Skytrain Station pick-up & drop-off only *",},
     ],
   },
   {
-    title: 'Prepaid Package of Lessons (Each Lesson is 1.5 Hours)',
+    title: "Prepaid Packages of Lessons (Each Lesson is 1 Hour)",
+    note: "Note: Pick Up & Drop Off at Lansdowne Skytrain Station",
     rates: [
-      { amount: 950, description: '10 Lessons (You save 50$)' },
-      { amount: 1800, description: '20 Lessons (You save 200$)' },
-    ]
+      { amount: 600, description: "10 Lessons (You save $200)", note: "(1 Hour each X $60)", },
+      { amount: 1100, description: "20 Lessons (You save $500)", note: "(1 Hour each X $55)", },
+      { amount: 1500, description: "30 Lessons (You save $900)", note: "(1 Hour each X $50)",},
+    ],
   },
   {
-    title: 'ICBC Road Test Package',
+    title: "ICBC Road Test Package",
     rates: [
-      { amount: 150, description: 'ICBC road test preparation - 2 hours' },
-      { amount: 300, description: 'Car rental for ICBC Road Test' },
-    ]
+      { amount: 150, description: "ICBC road test preparation - 2 hours" },
+      { amount: 300, description: "Car rental for ICBC Road Test" },
+    ],
   },
-]
-
+];
 </script>
--- models/rate.ts
+++ models/rate.ts
@@ -2,4 +2,5 @@ export interface Rate {
   amount: number
   description: string
   discount?: number
+  note?: string
 }
