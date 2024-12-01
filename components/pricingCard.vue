0c6d6b79a495c7593fa2c78ec548f646cc78b0a5
Elina Kim <elinakim791@gmail.com>
2024-12-01 11:51:29 -0800

    adjust height of pricing card header for large screens

--- components/pricingCard.vue
+++ components/pricingCard.vue
@@ -2,13 +2,13 @@
   <div
     class="flex flex-col bg-white border border-gray-200 shadow-sm rounded-xl p-4 md:p-5"
   >
-    <div class="text-center m-4 h-22 md:h-20">
+    <div class="text-center mb-2 lg:mb-6 h-22 lg:h-24">
       <span class="text-2xl font-bold text-gray-800">{{ title }}</span>
       <p class="text-sm font-bold text-gray-500 mt-2">{{ note }}</p>
     </div>
     <div class="py-4">
       <ul>
-        <li v-for="rate in rates" class="mb-4 h-20">
+        <li v-for="rate in rates" class="mb-4 h-22">
           <div class="divider"></div>
           <div class="grid grid-cols-2 items-center">
             <span class="text-left">
